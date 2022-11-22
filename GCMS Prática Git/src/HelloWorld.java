e7acf5081831f82b68a23c34bde3a921cc4f1592
PeterNV <114534711+PeterNV@users.noreply.github.com>
2022-11-22 16:30:11 -0300

    NOVO COMMIT

--- GCMS Prática Git/src/HelloWorld.java
+++ GCMS Prática Git/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Pedro Vila Nova!!!");
 		System.out.println("Flavio Vila Nova!!!");
 		System.out.println("Carla Vila Nova!!!");
+		System.out.println("Lucas Peixoto!!!");
 	}
 }
